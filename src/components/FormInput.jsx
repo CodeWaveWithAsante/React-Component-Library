cbf3de2d80e6c71fb408297c542f5cabf3bd7218
CodeWaveWithAsante <132752907+CodeWaveWithAsante@users.noreply.github.com>
2023-06-03 15:18:26 +0530

    Update FormInput.jsx
    Form Input with AN icon

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -11,6 +11,8 @@ const FormInput = ({
   name,
   error,
   resetError,
+  leftIcon,
+  rightIcon,
 }) => {
   const [isValid, setIsValid] = useState(true);
 
@@ -28,16 +30,31 @@ const FormInput = ({
       <label className='block mb-2 text-sm font-medium text-gray-800'>
         {label}
       </label>
-      <input
-        onFocus={resetError}
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleInputChange}
-        required={required}
-        className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-2 focus:ring-blue-200 focus:border-blue-500 block w-full p-2.5 outline-none '
-      />
+      <div className='relative'>
+        {leftIcon && (
+          <div className='absolute inset-y-0 letf-0 flex items-center pl-3 pointer-events-none'>
+            {leftIcon}
+          </div>
+        )}
+        <input
+          onFocus={resetError}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleInputChange}
+          required={required}
+          className={`bg-gray-50 border ${
+            error ? "border-red-600" : "border-gray-300"
+          }  text-gray-900 text-md rounded-lg focus:ring-2 focus:ring-blue-200 focus:border-blue-500 block
+           w-full ${leftIcon && "pl-10"} p-2.5 outline-none `}
+        />
+        {rightIcon && (
+          <div className='absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer'>
+            {rightIcon}
+          </div>
+        )}
+      </div>
       {!isValid?.isValid && error === null && (
         <span className='mt-2 text-sm text-red-600'>{isValid?.msg}</span>
       )}
